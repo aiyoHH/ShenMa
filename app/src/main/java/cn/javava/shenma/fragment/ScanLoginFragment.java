053b89bac26d48860fbf6ebcbcfef42380795931
aiyoRui <liaozhihui@reachjunction.com>
2018-01-15 18:39:29 +0800

    [code]dialogFragment for each dialog in the future

---
+++ app/src/main/java/cn/javava/shenma/fragment/ScanLoginFragment.java
@@ -0,0 +1,17 @@
+package cn.javava.shenma.fragment;
+
+import android.app.DialogFragment;
+
+/**
+ * Description {des}
+ *
+ * @author Li'O
+ * @date 2018/1/15
+ * Todo {TODO}.
+ */
+
+public class ScanLoginFragment extends DialogFragment {
+
+
+
+}
