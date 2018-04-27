82fb94713cf89059cd1a11a2708ec69eaed022f3
aiyoRui <liaozhihui@reachjunction.com>
2018-04-27 22:55:28 +0800

    [code] 开机延长30秒

--- app/src/main/java/cn/javava/shenma/act/SplashActivity.java
+++ app/src/main/java/cn/javava/shenma/act/SplashActivity.java
@@ -27,6 +27,6 @@ public class SplashActivity extends AppCompatActivity{
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 finish();
             }
-        },10*1000);
+        },30*1000);
     }
 }
